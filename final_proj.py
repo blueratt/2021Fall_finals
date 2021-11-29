c536a5c79a9534668ef06eb050cb9178dabd7171
Shihao Jin <33336271+blueratt@users.noreply.github.com>
2021-11-29 15:58:11 -0600

    Test Commit

---
+++ final_proj.py
@@ -0,0 +1,11 @@
+# final_project
+# 597 PR final project
+# Created by Shihao Jin, Frencis Feng, Yanmeng Xin
+
+import random
+import pandas
+
+def get_cost_for_movie(movie_type: str):
+    if movie_type == 'Action':
+    elif movie_type == 'Comedy':
+    elif movie_type
